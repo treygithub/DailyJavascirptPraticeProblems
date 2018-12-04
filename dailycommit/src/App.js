d74a7fcefb79ba4c42502a7301a3a45bf07480fb
robert hunnicutt <robhunn74@gmail.com>
2018-12-04 11:24:49 -0600

    update

--- dailycommit/src/App.js
+++ dailycommit/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state={
+    init=""
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <input className="tx" type="text"/>
+        <input className="btn"
+          type = "button" 
+          value = "SUBMIT IT FAM" 
+          onClick = "()"
+          />
       </div>
     );
   }
